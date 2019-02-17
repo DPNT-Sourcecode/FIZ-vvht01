a0571c851b06c7ef9db1372e2a8561d48cf5a874
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-17 22:53:45 +0000

    Sun Feb 17 22:53:45 UTC 2019

---
+++ src/test/scala/befaster/solutions/HLO/HelloTest.scala
@@ -0,0 +1,12 @@
+package befaster.solutions.HLO
+
+import befaster.solutions.SUM.Sum
+import org.scalatest.{FlatSpec, Matchers}
+
+class HelloTest  extends FlatSpec with Matchers {
+
+  it should "test hello world" in {
+    Hello.hello("anjan")
+  }
+
+}
