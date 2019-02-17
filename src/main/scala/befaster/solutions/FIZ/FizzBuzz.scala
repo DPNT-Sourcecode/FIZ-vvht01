d3c2a13e2289c5dd0f89fd941de526428f1e5ed2
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-17 23:43:32 +0000

    Sun Feb 17 23:43:32 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -4,11 +4,12 @@ import befaster.runner.SolutionNotImplementedException
 
 object FizzBuzz {
   def fizzBuzz(number: Int): String =
-    if (number % 3== 0) "fizz"
+    if (number % 3 ==0 && number % 5 ==0 ) "fizz buzz"
+    else if (number % 3== 0) "fizz"
     else if (number % 5 == 0) "buzz"
-    else if (number % 3 ==0 && number % 5 ==0 ) "fizz buzz"
     else ""
 }
+
 
 
 
