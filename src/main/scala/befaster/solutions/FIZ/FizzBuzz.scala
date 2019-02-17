30777d5c175cf3828ef459ca00f56781a748b288
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-17 23:36:28 +0000

    Sun Feb 17 23:36:28 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -5,6 +5,8 @@ import befaster.runner.SolutionNotImplementedException
 object FizzBuzz {
   def fizzBuzz(number: Int): String =
     if (number % 3== 0) "fizz"
+    else if (number % 5 == 0) "buzz"
     else ""
 }
+
 
