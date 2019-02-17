97aa84e4cdfb923ba7a2fc4574686cbfdb9b7f95
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-17 23:24:28 +0000

    Sun Feb 17 23:24:28 UTC 2019

---
+++ src/test/scala/befaster/solutions/FIZ/FizzBuzzTest.scala
@@ -0,0 +1,11 @@
+package befaster.solutions.FIZ
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class FizzBuzzTest  extends FlatSpec with Matchers {
+
+  it should "test hello world" in {
+    //Hello.hello("anjan") shouldBe "Hello, anjan!"
+  }
+
+}
