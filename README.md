832f8e8dc9a9838782ebb0b889bda8cf217f6103
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-17 22:26:16 +0000

    Sun Feb 17 22:26:16 UTC 2019

---
+++ README.md
@@ -0,0 +1,12 @@
+# tdl-runner-scala
+
+
+## 1. Requirements
+
+- `Scala 2.12.3`
+- `sbt 0.13.8`
+
+## 2. How to start
+
+- Open `src/main/scala/befaster/SendCommandToServer.scala`
+- Read the comments as documentation, they will guide through the rest of the setup
---
+++ get_coverage_for_challenge.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+CHALLENGE_ID=$1
+SCOVERAGE_REPORT_XML_FILE="${SCRIPT_CURRENT_DIR}/target/scala-2.12/scoverage-report/scoverage.xml"
+SCALA_CODE_COVERAGE_INFO="${SCRIPT_CURRENT_DIR}/coverage.tdl"
+
+( cd ${SCRIPT_CURRENT_DIR} && sbt clean coverage tdlTests coverageReport )
+
+[ -e ${SCALA_CODE_COVERAGE_INFO} ] && rm ${SCALA_CODE_COVERAGE_INFO}
+
+if [ -f "${SCOVERAGE_REPORT_XML_FILE}" ]; then
+    COVERAGE_PERCENT=$(( 0 ))
+    echo ${COVERAGE_PERCENT} > ${SCALA_CODE_COVERAGE_INFO}
+    COVERAGE_OUTPUT=$(xmllint --xpath '//package[@name="befaster.solutions.'${CHALLENGE_ID}'"]/@statement-rate' ${SCOVERAGE_REPORT_XML_FILE})
+    if [[ ! -z "${COVERAGE_OUTPUT}" ]]; then
+        COVERAGE_PERCENT=$(echo ${COVERAGE_OUTPUT} | tr '".' ' ' | tr -s ' ' | awk '{printf "%.0f", $2}')
+        COVERAGE_PERCENT=$(( ${COVERAGE_PERCENT} + 0 ))
+    fi
+    echo ${COVERAGE_PERCENT} > ${SCALA_CODE_COVERAGE_INFO}
+    cat ${SCALA_CODE_COVERAGE_INFO}
+    exit 0
+else
+    echo "No coverage report was found"
+    exit -1
+fi
