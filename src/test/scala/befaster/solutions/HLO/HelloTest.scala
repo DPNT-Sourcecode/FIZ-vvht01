c283c6bbbcce8900b47f1bd909ae04eb3c493ff3
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-17 23:03:03 +0000

    Sun Feb 17 23:03:03 UTC 2019

--- src/test/scala/befaster/solutions/HLO/HelloTest.scala
+++ src/test/scala/befaster/solutions/HLO/HelloTest.scala
@@ -6,9 +6,10 @@ import org.scalatest.{FlatSpec, Matchers}
 class HelloTest  extends FlatSpec with Matchers {
 
   it should "test hello world" in {
-    Hello.hello("anjan") shouldBe "Hello, World!"
+    Hello.hello("anjan") shouldBe "Hello, anjan!"
   }
 
 }
+
 
 
