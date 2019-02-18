8d0111995ac1e6fbcb0b9fa1ee07fb6920afa4bc
sbx_user1060 <sbx_user1060@ip-10-129-36-69.eu-west-2.compute.internal>
2019-02-18 00:32:47 +0000

    Mon Feb 18 00:32:47 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -6,12 +6,14 @@ object FizzBuzz {
   def fizzBuzz(number: Int): String = {
 
     val pattern3 = "[3-3]"
+    val pattern5 = "[5-5]"
 
     if (number % 3 == 0 && number % 5 == 0) "fizz buzz"
     else if (number % 3 == 0 || pattern3.matches(number.toString) == true) "fizz"
-    else if (number % 5 == 0) "buzz"
+    else if (number % 5 == 0  || pattern5.matches(number.toString) == true) "buzz"
     else number.toString
   }
 
 }
+
 
